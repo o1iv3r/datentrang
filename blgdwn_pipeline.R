addd3ff4bcfff594950a76b3c959aa8a4ebb35e8
Oliver Pfaffel <opfaffel@gmail.com>
2019-06-20 16:57:57 +0200

    First commit

---
+++ blgdwn_pipeline.R
@@ -0,0 +1,12 @@
+library(blogdown)
+# https://themes.gohugo.io/
+# blogdown::new_site(theme = "vimux/mainroad")
+# install_theme("bake/solar-theme-hugo")
+
+## Commit changes
+# terminal: git commit -m "Commit message"
+# or commit button in Rstudio
+
+## Push to github
+# terminal: git push -u origin master
+# or push button in Rstudio
