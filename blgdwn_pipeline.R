620b71540e3d94cfce877e86a409cfce4f4fbe59
Oliver Pfaffel <opfaffel@gmail.com>
2020-09-06 14:25:46 +0200

    added notes

--- blgdwn_pipeline.R
+++ blgdwn_pipeline.R
@@ -18,4 +18,16 @@ library(blogdown)
 # terminal: git push -u origin master
 # or push button in Rstudio
 
+
+#### Notes ####
+
+# add applause button to next blog post!!!
+# ```{r applause-button, echo=FALSE}
+# applause::button(
+#   color = "#0C4047",
+#   width = "100px"
+# )
+# ```
+
+
 # JS support: https://www.r-consortium.org/blog/2020/08/30/from-r-hub-javascript-for-the-r-package-developer
